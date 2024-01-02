02d3da79cb8121dd68d9b989140f174168c55daa
Dmitry0x0 <volov.dmit@yandex.ru>
2024-01-03 02:48:44 +0300

    feat: builds

--- src/tests/index.ts
+++ src/tests/index.ts
@@ -1,11 +1,11 @@
-import RunIpfsNode from "..";
+import RunIpfsNode from '..';
 
 const start = async () => {
   const ipfsNode = await RunIpfsNode.run();
-  const buffer = Buffer.from(new TextEncoder().encode("Hello world!"));
+  const buffer = Buffer.from(new TextEncoder().encode('Hello world!'));
   const cid = await ipfsNode.push(buffer);
   const res = await ipfsNode.fetch(cid);
-  if (res.toString() !== buffer.toString()) throw new Error("> Test fail!");
+  if (res.toString() !== buffer.toString()) throw new Error('> Test fail!');
   return cid;
 };
 
@@ -15,6 +15,6 @@ start()
     process.exit(1);
   })
   .then((e) => {
-    console.log("> Test passed successfully.", e);
+    console.log('> Test passed successfully.', e);
     process.exit(0);
   });
